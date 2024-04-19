48fbaa58bccd1da2d1693a9ca4fee8bcd6b5f824
Mehulparekh144 <mehulparekh144@gmail.com>
2024-04-19 19:08:48 -0400

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import {Route, Routes} from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import axios from 'axios';
+import { Toaster } from 'react-hot-toast';
+import Profile from './pages/Profile';
+import ProtectedRoute from './components/ProtectedRoute';
+import ProfileEdit from './pages/Profile/Edit';
+import { UserProvider } from './context/userContext';
+import BloggerRoute from './components/BloggerRoute';
+import Post from './pages/Home/Post';
+import Rate from './pages/Rate';
+import TotalReviews from './pages/Rate/TotalReviews';
+import Search from './pages/Search';
+import Details from './pages/Search/Details';
+import FollowingList from './pages/Profile/Following';
+import Followers from './pages/Profile/Followers';
+import IndividualProfile from './pages/Profile/Individual';
 
 function App() {
+  axios.defaults.baseURL =
+    "http://localhost:4000/api" || process.env.REACT_APP_BACKEND_URL;
+  axios.defaults.withCredentials = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <UserProvider>
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            style: {
+              background: "#333",
+              color: "#fff",
+            },
+          }}
+        />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<ProtectedRoute Component={Profile}/>} />
+          <Route path="/profile/edit/:id" element={<ProtectedRoute Component={ProfileEdit}/>} />
+          <Route path="/profile/:pid" element={<IndividualProfile/>} />
+          <Route path="/profile/following" element={<ProtectedRoute Component={FollowingList}/>} />
+          <Route path="/profile/followers" element={<ProtectedRoute Component={Followers}/>} />
+          <Route path="/post" element={<BloggerRoute Component={Post}/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/rate/:mid" element={<ProtectedRoute Component={Rate} />} />
+          <Route path="/reviews/:mid" element={<TotalReviews />} />
+          <Route path="/search" element={<Search/>} />
+          <Route path='/details/:title' element={<Details/>}/>
+         </Routes>
+      </UserProvider>
+    </>
   );
 }
 
